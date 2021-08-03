b0107feb6ec31deb7a501b7e09a246330ddc9d98
Martin <martinkos007@gmail.com>
2021-08-04 00:13:56 +0200

    Creating instance of SoundController to preload Clip

--- src/ml/codeboy/engine/exampleGames/ExampleGames.java
+++ src/ml/codeboy/engine/exampleGames/ExampleGames.java
@@ -1,11 +1,12 @@
 package ml.codeboy.engine.exampleGames;
 
 import ml.codeboy.engine.Sound.Sound;
+import ml.codeboy.engine.Sound.SoundController;
 import ml.codeboy.engine.exampleGames.menu.Menu;
 
 public class ExampleGames {
     public static void main(String[] args) {
         new Menu();
-        Sound.createSound("Epic_Journey.wav");
+        SoundController.getInstance();
     }
 }
